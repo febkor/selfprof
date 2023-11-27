df5628734c2083d5e5595d1e2c9fe15135f19c91
Febin <25066330+febkor@users.noreply.github.com>
2023-11-27 23:09:47 +0100

    Update capture logic for name lookback scheme

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::{
     collections::VecDeque,
     ffi::CString,
-    mem::size_of,
     path::Path,
     thread,
     time::{self},
@@ -11,8 +10,7 @@ use selfprof::{active_app_name, idle_time, storage, time_now, Snap};
 
 mod cli;
 
-type Name = String;
-type NameId = u32;
+type Name = CString;
 
 fn main() {
     let config = cli::parse();
@@ -31,34 +29,64 @@ fn main() {
     let dir_path = &Path::new(&config.out_dir);
     std::fs::create_dir_all(dir_path).expect("create output directory");
     let snaps_path = dir_path.join("selfprof.dat");
-    let snaps = storage::load_snaps(&snaps_path);
 
+    const MAX_NAME_LOOKBACK: usize = u8::MAX as usize;
     let mut snaps: Vec<Snap> = Vec::with_capacity(snaps_per_save);
-    const MAX_NAME_LOOKBACK: usize = size_of::<u8>();
     let mut names: VecDeque<Name> = VecDeque::with_capacity(MAX_NAME_LOOKBACK);
 
+    {
+        // Load snaps to initialize names
+        let snaps_prev = storage::load_snaps(&snaps_path);
+        for snap in snaps_prev {
+            if config.verbose {
+                println!("Loaded {:?}", snap);
+            }
+
+            if names.len() == MAX_NAME_LOOKBACK {
+                names.pop_front();
+            }
+            names.push_back(snap.name);
+        }
+    }
+
     loop {
-        for _ in 1..=snaps_per_save {
+        for _ in 0..snaps_per_save {
             thread::sleep(interval);
             let idle = idle_time();
             if idle > config.idle_cutoff {
                 continue;
             }
+            let time = time_now();
             let name = active_app_name();
-
-            // TODO: find rindex (reverse!)
-            let index = names.iter().position(|x| x == &name).unwrap_or(0);
+            let index: u8 = names
+                .iter()
+                .rev()
+                .position(|x| x.as_bytes() == name.as_bytes())
+                .unwrap_or(MAX_NAME_LOOKBACK)
+                .try_into()
+                .unwrap_or(MAX_NAME_LOOKBACK as u8);
+            let name_exists = index < MAX_NAME_LOOKBACK as u8;
 
             let snap = Snap {
-                time: time_now(),
-                idle: idle,
-                index: index.try_into().unwrap_or(0),
-                // TODO: only write if index == 0
-                name: CString::new(name).expect("No nul"),
+                time,
+                idle,
+                index,
+                name: if name_exists {
+                    CString::default()
+                } else {
+                    CString::new(name.clone()).expect("No nul")
+                },
             };
 
             if config.verbose {
                 println!("{:?}", &snap);
+            }
+
+            if !name_exists {
+                if names.len() == MAX_NAME_LOOKBACK {
+                    names.pop_front();
+                }
+                names.push_back(CString::new(name.clone()).expect("No nul"));
             }
 
             snaps.push(snap);
@@ -70,7 +98,6 @@ fn main() {
 
         storage::update_snaps(&snaps, &snaps_path, |s| s.to_bytes());
 
-        names.clear();
         snaps.clear();
     }
 }
